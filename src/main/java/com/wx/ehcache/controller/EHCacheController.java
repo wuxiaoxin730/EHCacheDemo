745327bd301ec33d469efac2a0fa812bcb73857c
wuxin53 <wuxin53@crc.com.hk>
2017-11-28 17:51:10 +0800

    解决使用JAR启动后，访问页面无法找到模板文件的问题

--- src/main/java/com/wx/ehcache/controller/EHCacheController.java
+++ src/main/java/com/wx/ehcache/controller/EHCacheController.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.thymeleaf.util.StringUtils;
 
 import java.util.List;
 
--- src/main/java/com/wx/ehcache/thymeleaf/ThymeleafConfiguration.java
+++ src/main/java/com/wx/ehcache/thymeleaf/ThymeleafConfiguration.java
@@ -10,50 +10,47 @@ import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 import org.thymeleaf.templatemode.TemplateMode;
 
+
 @Configuration
 public class ThymeleafConfiguration implements ApplicationContextAware {
     private ApplicationContext applicationContext;
-
-    @Bean
-    public SpringResourceTemplateResolver templateResolver(){
-        // SpringResourceTemplateResolver automatically integrates with Spring's own
-        // resource resolution infrastructure, which is highly recommended.
-        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
-        templateResolver.setApplicationContext(this.applicationContext);
-        templateResolver.setPrefix("/templates/");
-        templateResolver.setSuffix(".html");
-        // HTML is the default value, added here for the sake of clarity.
-        templateResolver.setTemplateMode(TemplateMode.HTML);
-        // Template cache is true by default. Set to false if you want
-        // templates to be automatically updated when modified.
-        templateResolver.setCacheable(true);
-        return templateResolver;
-    }
-
-    @Bean
-    public SpringTemplateEngine templateEngine(){
-        // SpringTemplateEngine automatically applies SpringStandardDialect and
-        // enables Spring's own MessageSource message resolution mechanisms.
-        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-        templateEngine.setTemplateResolver(templateResolver());
-        // Enabling the SpringEL compiler with Spring 4.2.4 or newer can
-        // speed up execution in most scenarios, but might be incompatible
-        // with specific cases when expressions in one templates are reused
-        // across different data types, so this flag is "false" by default
-        // for safer backwards compatibility.
-        templateEngine.setEnableSpringELCompiler(true);
-        return templateEngine;
-    }
-
-    @Bean
-    public ThymeleafViewResolver viewResolver(){
-        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
-        viewResolver.setTemplateEngine(templateEngine());
-        // NOTE 'order' and 'viewNames' are optional
-//        viewResolver.setOrder(1);
-//        viewResolver.setViewNames(new String[] {".html", ".xhtml"});
-        return viewResolver;
-    }
+//
+//    @Bean
+//    public SpringResourceTemplateResolver templateResolver(){
+//        // SpringResourceTemplateResolver automatically integrates with Spring's own
+//        // resource resolution infrastructure, which is highly recommended.
+//        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+//        templateResolver.setApplicationContext(this.applicationContext);
+////        templateResolver.setPrefix("/pages/");
+////        templateResolver.setSuffix(".html");
+//        templateResolver.setTemplateMode(TemplateMode.HTML);
+//        // Template cache is true by default. Set to false if you want
+//        // templates to be automatically updated when modified.
+//        templateResolver.setCacheable(true);
+//        return templateResolver;
+//    }
+//
+//    @Bean
+//    public SpringTemplateEngine templateEngine(){
+//        // SpringTemplateEngine automatically applies SpringStandardDialect and
+//        // enables Spring's own MessageSource message resolution mechanisms.
+//        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+//        templateEngine.setTemplateResolver(templateResolver());
+//        // Enabling the SpringEL compiler with Spring 4.2.4 or newer can
+//        // speed up execution in most scenarios, but might be incompatible
+//        // with specific cases when expressions in one templates are reused
+//        // across different data types, so this flag is "false" by default
+//        // for safer backwards compatibility.
+//        templateEngine.setEnableSpringELCompiler(true);
+//        return templateEngine;
+//    }
+//
+//    @Bean
+//    public ThymeleafViewResolver viewResolver(){
+//        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+//        viewResolver.setTemplateEngine(templateEngine());
+//        return viewResolver;
+//    }
 
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
