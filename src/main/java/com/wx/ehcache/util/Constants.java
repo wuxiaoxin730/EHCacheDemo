ac427c21e6e70cb6ac424797966aa960ee71cd32
Nicolas Wu <wuxiaoxin730@sina.com>
2017-11-28 22:44:12 +0800

    Enable JGroup Cluster

--- src/main/java/com/wx/ehcache/util/Constants.java
+++ src/main/java/com/wx/ehcache/util/Constants.java
@@ -13,5 +13,5 @@ public interface Constants {
 
     int DEFAULT_MAX_ENTRIES_LOCAL_HEAP = 100;
 
-    String CACHE_EVENT_LISTENER_FACTORY_CLASS = "net.sf.ehcache.distribution.RMICacheReplicatorFactory";
+    String CACHE_EVENT_LISTENER_FACTORY_CLASS = "net.sf.ehcache.distribution.jgroups.JGroupsCacheReplicatorFactory";
 }
